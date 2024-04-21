4833cb72f4affc9a9dc37ed371508fb063d90d7b
saifulaija <saifulaija2023@gmail.com>
2024-04-21 15:39:00 +0600

    reset password done

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,23 +1,24 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { userController } from './user.controller';
-import authGuard from '../../middlewares/authGuard';
-import { UserRole } from '@prisma/client';
-import { fileUploader } from '../../../helpers/fileUploader';
-import { userValidationSchema } from './user.validationSchema';
+import express, { Request, Response, NextFunction } from "express";
+
+import authGuard from "../../middlewares/authGuard";
+import { UserRole } from "@prisma/client";
+import { fileUploader } from "../../../helpers/fileUploader";
+import { userValidationSchema } from "./user.validationSchema";
+import { userController } from "./user.controller";
 
 const router = express.Router();
 
 router.post(
-   '/',
-   authGuard(UserRole.SUPER_ADMIN, UserRole.ADMIN),
-   fileUploader.upload.single('file'),
-   (req: Request, res: Response, next: NextFunction) => {
-      req.body = userValidationSchema.createAdminSchema.parse(
-         JSON.parse(req.body.data)
-      );
+  "/",
+  authGuard(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  fileUploader.upload.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = userValidationSchema.createAdminSchema.parse(
+      JSON.parse(req.body.data)
+    );
 
-      return userController.createAdmin(req, res, next);
-   }
+    return userController.createAdmin(req, res, next);
+  }
 );
 
 export const userRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { userRoutes } from '../modules/user/user.routes';
-import { AdminRoutes } from '../modules/admin/admin.routes';
+
 import { authRoutes } from '../modules/auth/auth.routes';
+import { userRoutes } from '../modules/User/user.routes';
+import { AdminRoutes } from '../modules/Admin/admin.routes';
 
 const router = express.Router();
 
